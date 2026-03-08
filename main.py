1a4aa6ef644aa8a39d62502d8592039cb0d7a9b6
PIeofheaven <ezio.nomad@outlook.com>
2026-03-08 15:36:38 +0500

    кфнррррр

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-meme_expl = {
-    "лол": "очень смешно",
-    "кринж": "что-то странное, стыдное",
-    "рофл": "шутка",
-    "щищ": "одобрение или восторг",
-    "криповый":  "страшный, пугающий",
-    "агриться": "злиться"
-    }
+from random import choice
 
-for i in range(5):
-    word_req = input("Input word that you want an explanation")
-    if word_req in meme_expl.keys():
-        print("значит " + meme_expl[word_req])
-    else:
-        print("word not in database")
+allowed_characters = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+password_req = int(input("введи длину пароля "))
+password = ''
+
+for i in range(password_req):
+    password += choice(allowed_characters)
+
+print (password)
